6d9cee2a422c7fe638cae9daae844c740decb6a9
Eric Russell <Erictim.Russell@live.com>
2023-02-02 09:58:53 -0700

    init push dependencies setup

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+    <header>
+      <!-- navbar -->
+    </header>
+
+    <div>
+      <slot />
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@pinia/nuxt'
+  ],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined'
+        }
+      ]
+    }
+  }
+})
